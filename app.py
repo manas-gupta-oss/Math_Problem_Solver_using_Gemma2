a79eb247ee93880f7715d5044ba54eceba1163b8
Manas Gupta <guptamanas657@gmail.com>
2026-07-29 01:32:43 +0530

    Initializing the Chatgroq Model

--- app.py
+++ app.py
@@ -9,4 +9,12 @@ from langchain.callbacks import StreamlitCallbackHandler
 
 #Setup up the streamlit app
 st.set_page_config(page_title="Text To math problem solver")
+st.title("Text to Math Problem Solver using Google Gemma2")
+
+groq_api_key=st.sidebar.text_input(label="Groq API Key",type="password")
+
+if not groq_api_key:
+    st.info("Please Add your Groq API Key to continue")
+
+llm=ChatGroq(model="Gemma-9b-It",groq_api_key=groq_api_key)
 
