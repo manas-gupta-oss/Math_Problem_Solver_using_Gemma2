1a997a8fefade6fed4412846bc1ea6af63a09cc6
Manas Gupta <guptamanas657@gmail.com>
2026-07-29 01:10:58 +0530

    Setting Up the streamlit UI

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit 
+import streamlit as st
 from langchain_groq import ChatGroq
 from langchain.chains import LLMMathChain,LLMChain
 from langchain_community.utilities import WikipediaAPIWrapper
@@ -6,4 +6,7 @@ from langchain.prompts import PromptTemplate
 from langchain.agents.agent_types import AgentType
 from langchain.agents import Tool, initialize_agent
 from langchain.callbacks import StreamlitCallbackHandler
+
+#Setup up the streamlit app
+st.set_page_config(page_title="Text To math problem solver")
 
